8308f2b4bf6ab15ab76921c6d94b702b29a0fa62
romtanik <romtanik@gmail.com>
2024-03-31 16:52:02 +0300

    add file

---
+++ Pet_Cat.py
@@ -0,0 +1,14 @@
+class Cat:
+    def __init__(self, name, genger, age):
+        self.name = name
+        self.genger = genger
+        self.age = age
+
+    def getName(self):
+        return self.name
+
+    def getGenger(self):
+        return self.genger
+
+    def getAge(self):
+        return self.age
---
+++ testCat.py
@@ -0,0 +1,11 @@
+from Pet_Cat import Cat
+
+r1 = Cat("Барон", "Мальчик", 2)
+r2 = Cat("Сэм", "Мальчик", 2)
+
+print("r1.name=", r1.name)
+print("r1.genger=", r1.genger)
+print("r1.age=", r1.age)
+print("r2.name=", r2.name)
+print("r2.genger=", r2.genger)
+print("r2.age=", r2.age)
